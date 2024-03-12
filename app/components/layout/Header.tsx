6c33069d2af12e6c908ca95d41afb1a3cb76e598
kietother <161268093+kietother@users.noreply.github.com>
2024-03-12 09:15:41 +0000

    Remove check pre

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -6,8 +6,6 @@ import Initial from "./Initial";
 import Logo from '@/public/assets/media/logo_testing.png';
 import Image from "next/image";
 import SessionProviderWrapper from "./SessionProviderWrapper";
-import { getEnumValueFromString } from "@/app/utils/HelperFunctions";
-import { ERoleType } from "@/app/models/enums/ERoleType";
 
 const DynamicLogoutButton = dynamic(() => import('./LogoutButton'), {
     ssr: true
@@ -108,8 +106,7 @@ export default async function Header() {
                                     <li>
                                         <a href="/schedule">{t('schedule')}</a>
                                     </li>
-                                    {process.env.MOBILE_URL && 
-                                        (getEnumValueFromString(session?.user?.token?.roles) === ERoleType.UserPremium || getEnumValueFromString(session?.user?.token?.roles) === ERoleType.UserSuperPremium) && (
+                                    {process.env.MOBILE_URL && (
                                         <li>
                                             <a href="/install">{t('install')}</a>
                                         </li>
