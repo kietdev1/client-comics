46f192774b688327032afcfa50ab496ea8156d62
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-03-22 21:14:29 +0700

    Keydown shorcut (#165)
    * Keydown shorcut next
    
    * Fix size paging
    
    ---------
    
    Co-authored-by: Kiet Tran <kiet.tran@tscmiami.com>
    Co-authored-by: khoingo99 <khoi.ngo@logixtek.com>

--- app/components/common/SupportButton.tsx
+++ app/components/common/SupportButton.tsx
@@ -1,11 +1,14 @@
 "use client"
 import SupportButtonProps from '@/app/models/common/SupportButtonProps';
 import { setHistory } from '@/app/utils/HelperFunctions';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const SupportButton: React.FC<SupportButtonProps> = ({ prevLink, nextLink}) => {
-    const [showControls, setShowControls] = useState(false);
+const SupportButton: React.FC<SupportButtonProps> = ({ prevLink, nextLink }) => {
+  const [showControls, setShowControls] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
+
+  const previousBtnRef = useRef<any>(null);
+  const nextBtnRef = useRef<any>(null);
 
   const toggleControls = () => {
     setShowControls(!showControls);
@@ -17,10 +20,28 @@ const SupportButton: React.FC<SupportButtonProps> = ({ prevLink, nextLink}) => {
     setIsVisible(window.scrollY > threshold && distanceFromBottom > 2800);
   };
 
+  const handleKeyDown = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'ArrowLeft':
+        // Handle logic for Arrow Left key press
+        previousBtnRef.current?.click();
+        break;
+      case 'ArrowRight':
+        // Handle logic for Arrow Right key press
+        nextBtnRef.current?.click();
+        break;
+      default:
+        break;
+    }
+  }
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
+    document.addEventListener('keydown', handleKeyDown);
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      document.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
 
@@ -40,69 +61,69 @@ const SupportButton: React.FC<SupportButtonProps> = ({ prevLink, nextLink}) => {
 
   return (
     <div style={{ position: 'fixed', bottom: '80px', left: '20px', zIndex: 999 }}>
-      {showControls && (
-        <div style={{ display: 'flex'}}>
-          <button
-            onClick={() => {
-              setHistory(prevLink);
-              window.location.href = prevLink;
-            }}
-            style={{
-                display: (isVisible && prevLink != null) ? 'inline-block' : 'none',
-                background: 'var(--color-primary)',
-                color: 'white',
-                padding: '10px 15px',
-                border: 'none',
-                borderRadius: '22px',
-                cursor: 'pointer',
-                transition: 'right 0.3s ease-in-out',
-                zIndex: '999',
-                marginRight: '10px'
-              }}
-          >
-             <i className="fas fa-arrow-left"></i>
-          </button>
-          <button
-            onClick={() => {
-              setHistory(nextLink);
-              window.location.href = nextLink;
-            }}
-            style={{
-                display: (isVisible && nextLink != null) ? 'inline-block' : 'none',
-                background: 'var(--color-primary)',
-                color: 'white',
-                padding: '10px 15px',
-                border: 'none',
-                borderRadius: '22px',
-                cursor: 'pointer',
-                transition: 'right 0.3s ease-in-out',
-                zIndex: '999'
-              }}
-          >
-             <i className="fas fa-arrow-right"></i>
-          </button>
-        </div>
-      )}
+      <div style={{ display: 'flex' }}>
+        <button
+          onClick={() => {
+            setHistory(prevLink);
+            window.location.href = prevLink;
+          }}
+          ref={prevLink != null ? previousBtnRef : null}
+          style={{
+            display: (showControls && prevLink != null) ? 'inline-block' : 'none',
+            background: 'var(--color-primary)',
+            color: 'white',
+            padding: '10px 15px',
+            border: 'none',
+            borderRadius: '22px',
+            cursor: 'pointer',
+            transition: 'right 0.3s ease-in-out',
+            zIndex: '999',
+            marginRight: '10px'
+          }}
+        >
+          <i className="fas fa-arrow-left"></i>
+        </button>
+        <button
+          onClick={() => {
+            setHistory(nextLink);
+            window.location.href = nextLink;
+          }}
+          ref={nextLink != null ? nextBtnRef : null}
+          style={{
+            display: (showControls && nextLink != null) ? 'inline-block' : 'none',
+            background: 'var(--color-primary)',
+            color: 'white',
+            padding: '10px 15px',
+            border: 'none',
+            borderRadius: '22px',
+            cursor: 'pointer',
+            transition: 'right 0.3s ease-in-out',
+            zIndex: '999'
+          }}
+        >
+          <i className="fas fa-arrow-right"></i>
+        </button>
+      </div>
       <button
-      id="scrollBtn"
-      onClick={toggleControls}
-      style={{
-        display: isVisible ? 'block' : 'none',
-        position: 'fixed',
-        bottom: '20px',
-        left: '20px',  // Adjusted to move the button to the bottom-right corner
-        background: 'var(--color-primary)',
-        color: 'white',
-        padding: '10px 15px',
-        border: 'none',
-        borderRadius: '22px',
-        cursor: 'pointer',
-        transition: 'right 0.3s ease-in-out',
-        zIndex: '999'
-      }}
-    >
-      <i className="far fa-life-ring"></i>
-    </button>
+        id="scrollBtn"
+        onClick={toggleControls}
+        style={{
+          display: isVisible ? 'block' : 'none',
+          position: 'fixed',
+          bottom: '20px',
+          left: '20px',  // Adjusted to move the button to the bottom-right corner
+          background: 'var(--color-primary)',
+          color: 'white',
+          padding: '10px 15px',
+          border: 'none',
+          borderRadius: '22px',
+          cursor: 'pointer',
+          transition: 'right 0.3s ease-in-out',
+          zIndex: '999'
+        }}
+      >
+        <i className="far fa-life-ring"></i>
+      </button>
     </div>
   );
 };
