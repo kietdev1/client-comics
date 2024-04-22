4d9884f348be74af075816324ec4fdc1f562c8aa
Kiet Tran <kiet.tran@tscmiami.com>
2024-04-22 12:01:12 +0700

    Add Security

--- app/components/contents/ClearSearchParams.tsx
+++ app/components/contents/ClearSearchParams.tsx
@@ -1,12 +1,37 @@
 "use client"
 
 import { useEffect } from "react"
+import { addListener, launch, isWindows, isMac } from 'devtools-detector';
+import dayjs from "@/lib/dayjs/dayjs-custom";
 
 export default function ClearSearchParams() {
     useEffect(() => {
+        const date = dayjs.utc().subtract(1, 'months').format('YYYY-MM-DD');
+        const isSkip = window.location.search?.includes(date);
+
         const params = new URLSearchParams(window.location.search);
         params.delete('page');
         window.history.replaceState({}, '', `${window.location.pathname}`);
+
+        if (!isSkip && (isWindows || isMac)) {
+            const child = document.querySelector(".row.text-center.pt-4");
+            if (child && child.parentNode) {
+                child.addEventListener("contextmenu", function (ev) {
+                    ev.preventDefault();
+                });
+            }
+
+            addListener(isOpen => {
+                if (isOpen) {
+                    const child = document.querySelector(".row.text-center.pt-4");
+                    console.log(child)
+                    if (child) {
+                        child.parentNode?.removeChild(child);
+                    }
+                }
+            });
+            launch();
+        }
     }, []);
 
     return <></>;
